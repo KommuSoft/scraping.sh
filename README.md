ef4864573d8700b873eb6d8c953a061954d54ecd
KommuSoft <vanonsem.willem@gmail.com>
2014-11-24 11:11:37 +1100

    reorganize

---
+++ README.md
@@ -0,0 +1,8 @@
+# Scraping.sh
+
+A repository that contains various scripts to scrape data from public websites that fail to provide a (well-documented) API or dump.
+
+## Statement
+
+
+## Building a scraper
--- citaten.net/processpage.sh
+++ citaten.net/processpage.sh
@@ -1,5 +1,6 @@
-page=$(cat "test.html" | tidy -asxhtml -numeric 2> /dev/null)
-page=$(echo 'www.citaten.net	FALSE	/	FALSE	0	citatennet	citatenmandje=%7C28218%7C24548%7C21769%7C23971%7C37868%7C27599%7C36173%7C33027%7C33074%7C32336%7C28798%7C26964%7C26180%7C34242%7C26484%7C21809%7C25889%7C38653%7C37014%7C31374%7C29788%7C33313%7C24857%7C36879%7C22191%7C33356%7C31733%7C23302%7C28813%7C26968%7C' | wget -q -S --load-cookies=/dev/stdin -O - "$1" | tidy -asxhtml -numeric 2> /dev/null)
+#page=$(cat "test.html" | tidy -asxhtml -numeric 2> /dev/null)
+basket=$(seq 28000 28029 | sed ':a;N;$!ba;s/\n/%7C/g')
+page=$(echo "www.citaten.net	FALSE	/	FALSE	0	citatennet	citatenmandje=%7C$basket%7C" | wget -q -S --load-cookies=/dev/stdin -O - "http://www.citaten.net/zoeken/citaten_in_mandje.html" 2>/dev/null | tidy -asxhtml -numeric 2> /dev/null)
 uls=$(echo "$page" | xmllint --html --xpath '//ul[@id="citatenrijen"]' - 2>/dev/null)
 ncts=$(echo "$uls" | xmllint --html --xpath 'count(//ul/li)' - 2>/dev/null)
 for i in `seq 1 $ncts` #
