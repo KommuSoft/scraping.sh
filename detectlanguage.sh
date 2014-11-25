26309a567d841c2290b771ff308038b7de38cbd3
KommuSoft <vanonsem.willem@gmail.com>
2014-11-26 01:20:58 +1100

    added remark

--- detectlanguage.sh
+++ detectlanguage.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#Language detection is sometimes useful to make decisions in scraping or to classify content
 cd "jsli/lib"; node -e "require('./loader.js').identify('$1').language;" -p
